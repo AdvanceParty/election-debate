e23249ca13881aa3c493016573f240b29323db94
Gil Fewster <hello@gilfewster.com>
2019-05-10 14:36:11 +1000

    html is now generated by python, ready to drop into debate page without further formatting

--- rnn/debate.py
+++ rnn/debate.py
@@ -40,41 +40,32 @@ config = parser.parse_args()
 
 bs = textgenrnn(
     'rnn/' + config.bs_model + "/textgenrnn_weights.hdf5",
-    name="shorten",
-    # weights_path='rnn/' + config.bs_model + "/textgenrnn_weights.hdf5",
-    # vocab_path='rnn/' + config.bs_model + "/textgenrnn_vocab.json",
-    # config_path='rnn/' + config.bs_model + "/textgenrnn_config.json"
+    name="shorten"
 )
 
 pm = textgenrnn(
     'rnn/' + config.pm_model + "/textgenrnn_weights.hdf5",
-    name="pm",
-    # weights_path='rnn/' + config.pm_model + "/textgenrnn_weights.hdf5",
-    # vocab_path='rnn/' + config.pm_model + "/textgenrnn_vocab.json",
-    # config_path='rnn/' + config.pm_model + "/textgenrnn_config.json"
+    name="pm"
 )
+
+models = [
+    {'model': bs, 'speaker': "SHORTEN", 'class_name': 'shorten'},
+    {'model': pm, 'speaker': "PRIME MINISTER", 'class_name': 'pm'}
+]
+
+interviewer = {
+    'speaker': "INTERVIEWER",
+    'class_name':  "interviewer"
+}
 
 
 def get_question():
     lines = open('rnn/questions.txt').read().splitlines()
-    question = random.choice(lines)
-    return (question)
+    return random.choice(lines)
 
 
-def format_line(str, model):
-    html_string = "<section>"
-    if model == pm:
-        speaker = 'MORRISON'
-    elif model == bs:
-        speaker = 'SHORTEN'
-    else:
-        speaker = model
-
-    return speaker + ': ' + str + '\n\n'
-
-
-def generate(model, prefix=''):
-    result = model.generate(
+def generate(model_info, prefix=''):
+    result = model_info['model'].generate(
         temperature=config.temp,
         prefix=prefix,
         max_gen_length=300 + len(prefix),
@@ -83,77 +74,90 @@ def generate(model, prefix=''):
     return result[0]
 
 
-def wrap_in_tag(content, tag_type, class=''):
-    tag = '<' + tag_name + ''
-
-    if len(class) > 0:
-        tag += 'class="' + class = '"'
-    tag += '>\n' + content + '\n</' + tag_type + '>\n'
-    return tag
-
-
-def format_as_html(content, speaker_name, speaker_tag_name='h2', class=''):
-
-    speaker_tag = wrap_in_tag(speaker_name, speaker_tag_name)
-    section_content = speaker_tag + content
-    return wrap_in_tag(section_content, 'section', class)
-
-
-def format_transcript(str):
-    return '\n----Transcript----\n' + str + '--------------------\n'
-
-
-def lastWords(str):
+def get_seed(str):
+    # todo | need a more robust solution
+    # extract content from a string to use as a seed for the generator
     words = str.split(" ")
     return ' '.join(words[2:4] + words[-3:])
 
 
+def wrap_in_tag(content, tag_type, class_name=''):
+    # tag = '<' + tag_type + ''
+
+    # if len(class_name) > 0:
+    #     tag += 'class="' + class_name + '"'
+    # tag += '>\n' + content + '\n</' + tag_type + '>\n'
+    # return tag
+    return f"<{tag_type} class='{class_name}'>{content}</{tag_type}>"
+
+
+def format_dialog_as_html(content, speaker, class_name='', speaker_tag='h2'):
+    speaker_tag = wrap_in_tag(speaker, speaker_tag)
+    section_content = speaker_tag + content
+    return wrap_in_tag(section_content, 'section', class_name)
+
+
+def get_q_and_a():
+
+    # todo | Could all be much DRYer though this function
+
+    lines = []
+
+    q = get_question()
+    seed = get_seed(q)
+    content = format_dialog_as_html(q, 'INTERVIEWER', 'interviewer')
+    lines.append({'seed': seed, 'content': content})
+
+    #  randomly reverse the models list (or don't)
+    #  to randomly choose who answers first
+    if random.random() >= .5:
+        models.reverse()
+
+    #  random chance that the other model will offer an answer as well
+    #  and, if the second model does offer an answer then
+    #  a random chance that the first model will retort
+    hasReply = random.random() < .75
+    hasRetort = hasReply and random.random() > .85
+
+    first_speaker = models[0]
+    second_speaker = models[1]
+
+    seed = get_seed(lines[-1]['seed'])
+    raw_content = generate(first_speaker, seed).replace(seed, '')
+    content = format_dialog_as_html(
+        raw_content, first_speaker['speaker'], first_speaker['class_name'])
+
+    lines.append({'seed': seed, 'content': content})
+
+    # todo | just set number of dialog segments as int, so we can
+    # todo | refactor all this if/else junk into a loop
+    if hasReply:
+        seed = get_seed(lines[-1]['seed'])
+        raw_content = generate(second_speaker, seed).replace(seed, '')
+        content = format_dialog_as_html(
+            raw_content, second_speaker['speaker'], second_speaker['class_name'])
+
+        lines.append({'seed': seed, 'content': content})
+
+    if hasRetort:
+        seed = get_seed(lines[-1]['seed'])
+        raw_content = generate(first_speaker, seed).replace(seed, '')
+        content = format_dialog_as_html(
+            raw_content, first_speaker['speaker'], first_speaker['class_name'])
+
+        lines.append({'seed': seed, 'content': content})
+
+    return lines
+
+
 def getAll():
-    transcript = ''
-
+    interview = []
     for x in range(0, config.generate_n):
-        firstAnswerDecider = random.random()
-        hasReply = random.random() < .75
-        hasRetort = hasReply and random.random() > .85
+        dbug = f"Generating question/answer {x} of {config.generate_n}."
+        print(dbug)
+        interview.append(get_q_and_a())
 
-        first_speaker = bs if firstAnswerDecider >= .5 else pm
-        second_speaker = pm if first == bs else bs
-
-        q = get_question()
-        # section = format_line(q, "QUESTION")
-
-        seed = lastWords(q)
-        response_raw = generate(first_speaker, seed)
-        response_clean = response_raw.replace(seed, '')
-
-        section += format_line(response_clean, first_speaker)
-
-        # print('seed: ' + seed)
-        # print('raw: ' + response_raw)
-        # print('clean: ' + response_clean)
-        # print('-------')
-
-        if hasReply:
-            seed = lastWords(response_raw)
-            response2_raw = generate(second_speaker, seed)
-            response2_clean = response2_raw.replace(seed, '')
-
-            section += format_line(response2_clean, second_speaker)
-
-        if hasRetort:
-            seed = lastWords(response2_raw)
-            response3_raw = generate(first_speaker, seed)
-            response3_clean = response3_raw.replace(seed, '')
-
-            section += format_line(response2_clean, second_speaker)
-
-        transcript += format_section(section)
-
-        # just so we can watch as it generates
-        # comment out when building big files or moving to production
-        # print(section)
-
-    return format_transcript(transcript)
+    return interview
 
 
 print(getAll())
